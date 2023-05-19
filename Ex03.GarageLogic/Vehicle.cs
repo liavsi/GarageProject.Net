08e1b3c7b22817434e529f27230d3ec43768de7b
Liav Simantov <liavsi@mta.ac.il>
2023-05-19 15:04:20 +0300

    work on Vehicle and Wheel

---
+++ Ex03.GarageLogic/Vehicle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ex03.GarageLogic
+{
+    public abstract class Vehicle
+    {
+        private readonly string r_ModelName;
+        private readonly string r_LicenseNumber;
+        private float m_RemainingEnergyPercentage;
+        private List<Wheel> m_Wheels;
+
+        public Vehicle(string i_ModelName, string i_LicenseNumber, float i_RemainingEnergyPercentage, int i_NumOfWheels, float i_MaxWheelPressure, string i_WheelsManufacture)
+        {
+            r_ModelName = i_ModelName;
+            r_LicenseNumber = i_LicenseNumber;
+            m_RemainingEnergyPercentage = i_RemainingEnergyPercentage;
+            m_Wheels = new List<Wheel>(i_NumOfWheels);
+            for (int i = 0; i < i_NumOfWheels; i++)
+            {
+                Wheel wheel = new Wheel(i_WheelsManufacture, i_MaxWheelPressure);
+                m_Wheels.Add(wheel);
+            } 
+        }
+    }
+}
---
+++ Ex03.GarageLogic/Wheel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ex03.GarageLogic
+{
+    public class Wheel
+    {
+        private readonly string m_Manufactur;
+        private readonly float m_MaxAirPressure;
+        private float m_CurrentAirPressure;
+
+        public Wheel(string i_manufacturerName, float i_maxAirPressure)
+        {
+            m_Manufactur = i_manufacturerName;
+            m_MaxAirPressure = i_maxAirPressure;
+        }
+
+        public void InflateWheel(float i_airAmount)
+        {
+            if (m_CurrentAirPressure + i_airAmount <=  m_MaxAirPressure)
+            {
+                m_CurrentAirPressure += i_airAmount;
+            }
+            else
+            {
+                throw new Exception();
+            }
+        }
+    }
+}
