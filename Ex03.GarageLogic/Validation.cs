71696b4c5f31a6baa81037ccb75d15abfe812a72
Liav Simantov <liavsi@mta.ac.il>
2023-05-24 23:05:18 +0300

    das

--- Ex03.GarageLogic/Validation.cs
+++ Ex03.GarageLogic/Validation.cs
@@ -7,14 +7,6 @@ namespace Ex03.GarageLogic
 {
     public class Validation
     {
-        public static void ValidFuelVehicle(Vehicle i_Vehicle)
-        {
-            if (!(i_Vehicle.Engine is FuelEngine))
-            {
-                throw new ArgumentException("The vehicle is not fuel based");
-            }
-        }
-
         public static void IsValidLicenseNumber(string i_NumberStr)
         {
             if(int.TryParse(i_NumberStr,out int number))
